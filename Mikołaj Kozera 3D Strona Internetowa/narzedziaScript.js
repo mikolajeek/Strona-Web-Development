90a60171f6e150307ef0791761eb52fae9dda838
mikolajeek <90982043+mikolajeek@users.noreply.github.com>
2024-04-03 17:06:01 +0200

    Add files via upload

---
+++ Mikołaj Kozera 3D Strona Internetowa/narzedziaScript.js
@@ -0,0 +1,119 @@
+let select1 = document.querySelector("#selectingBar1");
+let btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener('click', () => {
+    switch (select1.value) {
+        case 'HTML':
+            window.open('https://pl.wikipedia.org/wiki/HTML');
+            break;
+        case 'CSS':
+            window.open('https://pl.wikipedia.org/wiki/Kaskadowe_arkusze_styl%C3%B3w');
+            break;
+        case 'JavaScript':
+            window.open('https://pl.wikipedia.org/wiki/JavaScript');
+            break;
+        case 'Python':
+            window.open('https://pl.wikipedia.org/wiki/Python');
+            break;
+        case 'Java':
+            window.open('https://pl.wikipedia.org/wiki/Java');
+            break;
+        case 'Ruby':
+            window.open('https://pl.wikipedia.org/wiki/Ruby_(j%C4%99zyk_programowania)');
+            break;
+        case 'Swift':
+            window.open('https://pl.wikipedia.org/wiki/Swift_(j%C4%99zyk_programowania_LLVM)');
+            break;
+    }
+});
+
+
+
+
+let select2 = document.querySelector("#selectingBar2");
+let btn2 = document.querySelector("#btn2");
+
+btn2.addEventListener('click', () => {
+    switch (select2.value) {
+        case 'React.js':
+            window.open('https://pl.wikipedia.org/wiki/React.js');
+            break;
+        case 'Angular':
+            window.open('https://pl.wikipedia.org/wiki/Angular_(framework)');
+            break;
+        case 'Django':
+            window.open('https://pl.wikipedia.org/wiki/Django_(framework)');
+            break;
+        case 'Node.js':
+            window.open('https://pl.wikipedia.org/wiki/Node.js');
+            break;
+    }
+});
+
+
+
+
+
+
+
+let select3 = document.querySelector("#selectingBar3");
+let btn3 = document.querySelector("#btn3");
+
+btn3.addEventListener('click', () => {
+    switch (select3.value) {
+        case 'Git':
+            window.open('https://pl.wikipedia.org/wiki/Git_(oprogramowanie)');
+            break;
+        case 'GitHub':
+            window.open('https://pl.wikipedia.org/wiki/GitHub');
+            break;
+        case 'Bitbucket':
+            window.open('https://pl.wikipedia.org/wiki/Bitbucket');
+            break;
+        case 'GitLab':
+            window.open('https://pl.wikipedia.org/wiki/GitLab');
+            break;
+        case 'Merculiar':
+            window.open('https://pl.wikipedia.org/wiki/Mercurial');
+            break;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+let select4 = document.querySelector("#selectingBar4");
+let btn4 = document.querySelector("#btn4");
+
+btn4.addEventListener('click', () => {
+    switch (select4.value) {
+        case 'OWASP ZAP':
+            window.open('https://en.wikipedia.org/wiki/OWASP_ZAP');
+            break;
+        case 'Burp Suite':
+            window.open('https://en.wikipedia.org/wiki/Burp_Suite');
+            break;
+        case 'Nessus':
+            window.open('https://en.wikipedia.org/wiki/Nessus_(software)');
+            break;
+        case 'GitLab':
+            window.open('https://pl.wikipedia.org/wiki/GitLab');
+            break;
+        case 'Nmap':
+            window.open('https://pl.wikipedia.org/wiki/Nmap');
+            break;
+    }
+});
+
+
+
+
+
+
---
+++ Mikołaj Kozera 3D Strona Internetowa/zastosowaniaScript.js
@@ -0,0 +1,162 @@
+let b1 = document.querySelector("#separatePointsBtn1");
+let p1 = document.querySelector("#fullDescription1");
+
+b1.addEventListener('click', () => {
+    if (p1.classList.contains('hidden')) {
+        p1.classList.remove('hidden');
+        b1.innerText = 'Schowaj tekst';
+    }
+    else {
+        p1.classList.add('hidden');
+        b1.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+let b2 = document.querySelector("#separatePointsBtn2");
+let p2 = document.querySelector("#fullDescription2");
+
+b2.addEventListener('click', () => {
+    if (p2.classList.contains('hidden')) {
+        b2.innerText = 'Schowaj tekst';
+        p2.classList.remove('hidden');
+    }
+    else {
+        p2.classList.add('hidden');
+        b2.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+let b3 = document.querySelector("#separatePointsBtn3");
+let p3 = document.querySelector("#fullDescription3");
+
+b3.addEventListener('click', () => {
+    if (p3.classList.contains('hidden')) {
+        b3.innerText = 'Schowaj tekst';
+        p3.classList.remove('hidden');
+    }
+    else {
+        p3.classList.add('hidden');
+        b3.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+
+
+let b4 = document.querySelector("#separatePointsBtn4");
+let p4 = document.querySelector("#fullDescription4");
+
+b4.addEventListener('click', () => {
+    if (p4.classList.contains('hidden')) {
+        b4.innerText = 'Schowaj tekst';
+        p4.classList.remove('hidden');
+    }
+    else {
+        p4.classList.add('hidden');
+        b4.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+
+
+let b5 = document.querySelector("#separatePointsBtn5");
+let p5 = document.querySelector("#fullDescription5");
+
+b5.addEventListener('click', () => {
+    if (p5.classList.contains('hidden')) {
+        b5.innerText = 'Schowaj tekst';
+        p5.classList.remove('hidden');
+    }
+    else {
+        p5.classList.add('hidden');
+        b5.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+
+let b6 = document.querySelector("#separatePointsBtn6");
+let p6 = document.querySelector("#fullDescription6");
+
+b6.addEventListener('click', () => {
+    if (p6.classList.contains('hidden')) {
+        b6.innerText = 'Schowaj tekst';
+        p6.classList.remove('hidden');
+    }
+    else {
+        p6.classList.add('hidden');
+        b6.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+let b7 = document.querySelector("#separatePointsBtn7");
+let p7 = document.querySelector("#fullDescription7");
+
+b7.addEventListener('click', () => {
+    if (p7.classList.contains('hidden')) {
+        b7.innerText = 'Schowaj tekst';
+        p7.classList.remove('hidden');
+    }
+    else {
+        p7.classList.add('hidden');
+        b7.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+
+let b8 = document.querySelector("#separatePointsBtn8");
+let p8 = document.querySelector("#fullDescription8");
+
+b8.addEventListener('click', () => {
+    if (p8.classList.contains('hidden')) {
+        b8.innerText = 'Schowaj tekst';
+        p8.classList.remove('hidden');
+    }
+    else {
+        p8.classList.add('hidden');
+        b8.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+
+
+let b9 = document.querySelector("#separatePointsBtn9");
+let p9 = document.querySelector("#fullDescription9");
+
+b9.addEventListener('click', () => {
+    if (p9.classList.contains('hidden')) {
+        b9.innerText = 'Schowaj tekst';
+        p9.classList.remove('hidden');
+    }
+    else {
+        p9.classList.add('hidden');
+        b9.innerText = 'Dowiedz się więcej';
+    }
+})
+
+
+let b10 = document.querySelector("#separatePointsBtn10");
+let p10 = document.querySelector("#fullDescription10");
+
+b10.addEventListener('click', () => {
+    if (p10.classList.contains('hidden')) {
+        b10.innerText = 'Schowaj tekst';
+        p10.classList.remove('hidden');
+    }
+    else {
+        p10.classList.add('hidden');
+        b10.innerText = 'Dowiedz się więcej';
+    }
+})
